be0eb1d5212cef4940aea2a6b9dd5587860fcf56
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2017-09-20 14:32:46 -0700

    finished assignment 4

--- exercises.js
+++ exercises.js
@@ -1,13 +1,38 @@
 new Vue({
     el: '#exercise',
     data: {
-        myClass: true
+        effectClasses: {
+            highlight: false,
+            shrink: true
+        },
+        float: 'float',
+        userClass: '',
+        isVisible: true,
+        myStyle: {
+            width: '100px',
+            height: '150px',
+            backgroundColor: 'gray',
+        },
+        progressBar: {
+            width: '0px',
+            backgroundColor: 'red'
+        }
     },
     methods: {
         startEffect: function() {
-            setTimeout(function() {
-                this.myClass = true ? myClass = 'highlight' : myClass = 'shrink';
-            }, 3000);
+            var vm = this;
+            setInterval(function() {
+                vm.effectClasses.highlight = !vm.effectClasses.highlight;
+                vm.effectClasses.shrink = !vm.effectClasses.shrink;
+            }, 1000);
+        },
+        startProgress: function() {
+            var vm = this;
+            var width = 0;
+            setInterval(function() {
+                width = width + 10;
+                vm.progressBar.width = width + 'px';
+            }, 500)
         }
     }
 });
